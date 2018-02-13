e2b2e33408520ea666e58a8f6c6d487533aac07d
aadhilr <aadhilr92@gmail.com>
2018-02-13 20:49:43 +0530

    PayPal Integration done

---
+++ IDisplay/Controllers/PayPalController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IDisplay.Migrations;
+using IDisplay.Models;
+
+namespace IDisplay.Controllers
+{
+    public class PayPalController : Controller
+    {
+        // GET: PayPal
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandBox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var paypal = new PayPalModel(useSandBox);
+            paypal.item_name = product;
+            paypal.amount = totalPrice;
+
+            return View(paypal);
+        }
+    }
+}
